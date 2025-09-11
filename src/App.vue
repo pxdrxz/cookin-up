cbf6c9804a203e799fd7a00bb3bba4a9bc2b09b2
pxdrxz <pedrohenriquefrederi@gmail.com>
2025-09-11 11:54:23 -0300

    Primeiro commit Cookin'Up / Main Page + Banner

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script lang="ts"> //e possivel utilizar <script setup> que tem a mesma funcao do export default, assim deixando o codigo mais enxuto
+import Banner from './components/Banner.vue';
+
+export default {
+components: { Banner: Banner } //components->objeto / <Banner/> = Banner (logo "Banner : Banner")
+}
+</script>
+
+<template>
+ <Banner/>
+</template>
+
